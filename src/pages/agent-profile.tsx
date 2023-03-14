58a559f994ef80fb14025adbeb6226862c1c6fa0
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2023-03-14 23:52:21 +0000

    pieChart completed

---
+++ src/pages/agent-profile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const agentProfile = () => {
+  return (
+    <div>agent-profie</div>
+  )
+}
+
+export default agentProfile
---
+++ src/pages/create-property.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const createProperties
+  = () => {
+    return (
+      <div>createProperties
+
+      </div>
+    )
+  }
+
+export default createProperties
